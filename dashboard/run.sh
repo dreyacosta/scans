5ed71947343d7eb7b4c3d9f8bb5db971ebb57cf6
David Rey <david.rey.acosta@gmail.com>
2020-03-23 12:04:28 +0800

    Update Dashboard Docker definition

--- dashboard/run.sh
+++ dashboard/run.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-npm start
+echo "Application environment: $APP_ENV"
+
+if [ "$APP_ENV" == "test" ]; then
+  npm test
+elif [ "$APP_ENV" == "dev" ]; then
+  npm run dev
+else
+  npm build
+  npm start
+fi
