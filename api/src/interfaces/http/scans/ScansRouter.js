24839cac9d9bca91af5b8b652432f03d69d4a84d
David Rey <david.rey.acosta@gmail.com>
2020-03-23 17:06:54 +0800

    Update ScansRouter with repository

--- api/src/interfaces/http/scans/ScansRouter.js
+++ api/src/interfaces/http/scans/ScansRouter.js
@@ -1,8 +1,11 @@
 const { Router } = require('express');
 const respondWith = require('../respondWith');
+const ScansMongoRepository = require('../../../infrastructure/persistence/mongodb/scans/ScansMongoRepository');
 const ScansAPI = require('./ScansAPI');
 
-const scansAPI = new ScansAPI({});
+const scansAPI = new ScansAPI({
+  scansRepository: new ScansMongoRepository(),
+});
 
 class ScansRouter {
   static create() {
