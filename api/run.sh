554957622a5111f28d6f114c1616e0d3d4fa3ed2
David Rey <david.rey.acosta@gmail.com>
2020-03-23 14:09:31 +0800

    Update API Docker for test

--- api/run.sh
+++ api/run.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-npm start
+echo "Application environment: $APP_ENV"
+
+if [ "$APP_ENV" == "test" ]; then
+  npm test
+elif [ "$APP_ENV" == "dev" ]; then
+  echo "Running dev app..."
+else
+  npm start
+fi
