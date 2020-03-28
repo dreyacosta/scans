bfdd8e42aec22dca616333e6a0a3b42cda726868
David Rey <david.rey.acosta@gmail.com>
2020-03-28 15:49:49 +0800

    Dashboard add semantic ui

--- dashboard/pages/index.js
+++ dashboard/pages/index.js
@@ -1,32 +1,63 @@
+import React, { useState } from 'react';
 import Head from 'next/head';
+import { Container, Header, List, Menu, Icon } from 'semantic-ui-react';
 
-const Home = () => (
-  <div>
-    <Head>
-      <title>Create Next App</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+const Home = () => {
+  const [activeItem, setActiveItem] = useState('home');
 
-    <main>
-      <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
+  const handleItemClick = (event, { name }) => setActiveItem(name);
 
-      <p className="description">
-        Get started by editing <code>pages/index.js</code>
-      </p>
-    </main>
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-    <footer>
-      <a
-        href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
-      </a>
-    </footer>
-  </div>
-);
+      <Menu pointing secondary>
+        <Menu.Item>
+          <Icon name='circle outline' />
+          <b>GuardRails</b>
+        </Menu.Item>
+        <Menu.Menu position='right'>
+          <Menu.Item
+            name='submit'
+            active={activeItem === 'submit'}
+            onClick={handleItemClick}
+          />
+          <Menu.Item
+            name='scans'
+            active={activeItem === 'scans'}
+            onClick={handleItemClick}
+          />
+        </Menu.Menu>
+      </Menu>
+
+      <Container textAlign='center'>
+        <Header as='h1'>
+          Scans
+        </Header>
+
+        <Header as='h2'>
+          Scans your repositories to alert you of any vulnerabilities and security issues.
+        </Header>
+
+        <List bulleted>
+          <List.Item>Submit a new repository: Scan vulnerabilities in a new repository</List.Item>
+          <List.Item>See list scans: Check the list of repository scans and click on each scan to see the findings</List.Item>
+        </List>
+      </Container>
+    </div>
+  );
+};
+
+const ScanSerializer = {
+  toJSON: (scan) => {
+    return {
+      id: scan.id,
+      repositoryName: scan.repositoryName,
+    };
+  }
+};
 
 export default Home;
