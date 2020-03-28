d36488e3cffe3e93ba44a3d0aaf7849312dc93de
David Rey <david.rey.acosta@gmail.com>
2020-03-28 22:42:26 +0800

    API auto populate scans on start up

--- api/run.sh
+++ api/run.sh
@@ -5,7 +5,9 @@ echo "Application environment: $APP_ENV"
 if [ "$APP_ENV" == "test" ]; then
   npm test
 elif [ "$APP_ENV" == "dev" ]; then
-  npm start
-else
-  npm start
+  if [ -n "$APP_POPULATE" ]; then
+    npm run populate:scans -- --number $NUMBER_SCANS
+  else
+    npm run start
+  fi
 fi
