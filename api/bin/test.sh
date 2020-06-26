e20e2a6b8fab5506c61e2536b5f1192ee1292703
David Rey <david.rey.acosta@gmail.com>
2020-06-26 12:46:38 +0200

    Fix API test script

--- api/bin/test.sh
+++ api/bin/test.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-docker build --tag scans-api:latest .
-docker run --detach --name mongo --tmpfs /data/db mongo:4-xenial
-docker run --rm --tty --name scans-api-test --link mongo:mongo --env-file .env --env APP_ENV=test scans-api:latest
-docker rm -f mongo
+docker build --tag scans-api:latest . \
+&& docker run --detach --name mongo --tmpfs /data/db mongo:4-xenial \
+&& docker run --rm --tty --name scans-api-test --link mongo:mongo --env-file .env --env APP_ENV=test scans-api:latest \
+&& docker rm -f mongo \
+|| (docker rm -f mongo && exit 1)
